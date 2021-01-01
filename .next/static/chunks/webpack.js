b2fca962c73f7c55ff293de0f4f0506b3ff7b4cb
jemblonganvalley <jemblonganvalley@gmail.com>
2021-01-01 15:06:44 +0700

    update redirec

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "f705b67bb04f6f379930";
+/******/ 	var hotCurrentHash = "458b53eb7fb5753fad30";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
