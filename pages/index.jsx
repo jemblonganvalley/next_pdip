e2d0a2336e89283f5eebff70c79f244072a96a3c
jemblonganvalley <jemblonganvalley@gmail.com>
2021-01-06 18:49:08 +0700

    updte video

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,9 @@
 import { useEffect } from "react";
 
 const Index = ({ post }) => {
-  // useEffect(() => {
-  //   window.location.href = "https://pdiperjuangan.id";
-  // }, []);
+  useEffect(() => {
+    window.location.href = "https://pdiperjuangan.id";
+  }, []);
   return (
     <div>
       <h1>pdiperjuangan.id</h1>
