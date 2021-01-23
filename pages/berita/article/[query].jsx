3301749b16329df08987b955ca7c17dae58f6510
jemblonganvalley <jemblonganvalley@gmail.com>
2021-01-23 22:38:04 +0700

    resize image

--- pages/berita/article/[query].jsx
+++ pages/berita/article/[query].jsx
@@ -39,12 +39,12 @@ export default function Index({ data, name, id }) {
               property="og:image:secure_url"
               content={`https://data.pdiperjuangan.id/public${data.path}`}
             />
-            <meta property="og:image:width" content="300" />
-            <meta property="og:image:height" content="300" />
+            <meta property="og:image:width" content="200" />
+            <meta property="og:image:height" content="200" />
             <meta property="og:image:alt" content={data.title} />
-            <meta property="og:type" content="website" />
-            <meta content="og:image:type" property="image/*" />
-            <meta content="og:image:alt" property={data.title} />
+            <meta property="og:type" content="article" />
+            <meta property="og:image:type" content="image/*" />
+            <meta property="og:image:alt" content={data.title} />
             <meta property="og:locale" content="id_ID" />
             <title>Berita</title>
           </Head>
